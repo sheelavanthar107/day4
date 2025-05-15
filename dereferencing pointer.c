07ece256bea0f8bbb4938ed54ee9facb1f16bffd
Harshitha S <harshithah785@gmail.com>
2025-05-15 15:02:36 +0530

    Update dereferencing pointer.c

--- dereferencing pointer.c
+++ dereferencing pointer.c
@@ -3,9 +3,6 @@ int main() {
     int a = 1;
     int *ptr = &a; 
     printf("Value of a: %d\n", a);
-    printf("Address of a: %p\n", a);
-    printf("Value stored in ptr (address of a): %p\n", ptr);
-    printf("Value pointed to by ptr (dereferencing): %d\n", *ptr); 
+    printf("Value stored in ptr (address of a): %d\n", *ptr);
     return 0;
 }
-
