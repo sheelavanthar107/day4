621aeebae3c2e157a5c1b97f06271ffaeceee650
Harshitha S <harshithah785@gmail.com>
2025-05-16 15:55:37 +0530

    Create .h.c

---
+++ .h.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+          struct harshitha{
+              int age;
+              float weight;
+          };
+          struct dhanu{
+              int mark;
+              struct harshitha r1;
+          };
+          struct dhanu p1;
+          scanf("%d",&p1.mark);
+          scanf("%d",&p1.r1.age);
+          scanf("%d",&p1.r1.weight);
+}
