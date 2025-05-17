8d8bb4a1bc6f91eeffc1bcab4904ce48c9640904
Harshitha S <harshithah785@gmail.com>
2025-05-17 07:58:50 +0530

    Create Input string and its length using strlen().c

---
+++ Input string and its length using strlen().c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  // reads input including spaces
+    str[strcspn(str, "\n")] = '\0';
+    int length = strlen(str);
+    printf("Length of the string: %d\n", length);
+   return 0;
+}
+
